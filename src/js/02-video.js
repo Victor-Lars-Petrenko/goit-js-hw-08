11d4e468cbb44beb21623016cb170a30c30f7753
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2023-11-20 08:44:05 +0200

    idk

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,19 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
-const iframe = document.getElementById('vimeo-player');
+// const iframe = document.getElementById('vimeo-player');
 
-const player = new Player(iframe);
+// const player = new Player(iframe);
 
-try {
-    const currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time")) ?? 0;
-    player.setCurrentTime(currentTime);
-} catch {}
+// try {
+//     const currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time")) ?? 0;
+//     player.setCurrentTime(currentTime);
+// } catch {}
 
-player.on('timeupdate', throttle(evt => {
-        try {
-            const timeToSave = JSON.stringify(evt.seconds);
-            localStorage.setItem("videoplayer-current-time", timeToSave);
-        } catch {}
-    }, 1000)
-);
+// player.on('timeupdate', throttle(evt => {
+//         try {
+//             const timeToSave = JSON.stringify(evt.seconds);
+//             localStorage.setItem("videoplayer-current-time", timeToSave);
+//         } catch {}
+//     }, 1000)
+// );
